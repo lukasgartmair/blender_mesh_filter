4c06922b6fda7d38034c4c4f6110f94ccc100fad
Lukas Gartmair <Lukas_Gartmair@gmx.de>
2016-12-20 13:08:13 +0100

    Update blender_mesh_filter.py

--- blender_mesh_filter.py
+++ blender_mesh_filter.py
@@ -1,1 +1,9 @@
 
+import bpy    
+
+treshold = 3000
+
+for ob in bpy.context.scene.objects:
+    me = ob.data
+    ob.select = ob.type == 'MESH' and len(me.polygons) < treshold
+bpy.ops.object.delete()
